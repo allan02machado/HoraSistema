852004cd3fbd0c3e0c1019f5338bf0d7836eea23
allan02machado <70041162+allan02machado@users.noreply.github.com>
2020-10-07 19:26:39 -0300

    Este commit exibe a resposta ao exercício

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace HoraSistema
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("---Recebendo informações do sistema...");
+            DateTime horaSistema = DateTime.Now;
+            Console.WriteLine();
+            Console.WriteLine($"A data e horário atual é: {horaSistema}");
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # HoraSistema
 Resposta ao exercício "HoraSistema"
+
+**Enunciado**
+
+Exiba a data e a hora atual do sistema.
+
+**Como executar o programa**
+
+Abra o programa e digite:
+
+dotnet run
+
